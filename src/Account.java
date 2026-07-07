3839e0f5b1c433c0ad4f23c068d1d7b6dbab5051
David Heng <hengjinyongd@gmail.com>
2026-07-07 04:04:09 +0000

    Create Account class

---
+++ src/Account.java
@@ -0,0 +1,44 @@
+public class Account {
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+
+    public Account(String accountNumber, String ownerName, double balance){
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    //Getter
+    public String getAccountNumber(){
+        return this.accountNumber;
+    }
+    public String getOwnerName(){
+        return this.ownerName;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
+
+    //Setter 
+    public void setOwnerName(String ownerName){
+        this.ownerName = ownerName;
+    }
+    public void deposit(double value){
+        this.balance += value;
+    }
+    public void withdraw(double value){
+        if (value < 0){
+            System.out.println("Amount cannot be negative");
+        } else if (value > balance){
+            System.out.println("Balance is not enough");
+        } else {
+            this.balance -= value;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "Account Number: " + this.accountNumber + "\nOwner Name: " + this.ownerName + "\nBalance: " + this.balance; 
+    }
+}
--- src/MainConsole.java
+++ src/MainConsole.java
@@ -38,7 +38,7 @@ public class MainConsole{
                 break;
             }
         }
-        
+        scanner.close();
 
     }
 }
